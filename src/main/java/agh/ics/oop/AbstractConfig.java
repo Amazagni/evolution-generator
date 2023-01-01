2cd1a6b276b9ec5458bd75018f180c59bbcd7b22
Amazagni <marcinxkomputer@gmail.com>
2023-01-02 00:56:34 +0100

    three new configs

---
+++ src/main/java/agh/ics/oop/AbstractConfig.java
@@ -0,0 +1,13 @@
+package agh.ics.oop;
+
+public abstract class AbstractConfig {
+    private int startingNumberOfGrass;
+    private int dailyGrassGrowth;
+    private int startingEnergy;
+    private int dailyEnergyLoss;
+    private int grassEnergyGain;
+    private int energyUsedToCreateAnimal;
+    private int minEnergyToReproduce;
+
+
+}
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,18 +3,25 @@ package agh.ics.oop;
 import agh.ics.oop.gui.App;
 import javafx.application.Application;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class World {
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws IOException {
 //        TEST RUN WITHOUT GUI
 //        EarthMap map = new EarthMap(new Vector2d(15,15));
 //        SimulationEngine engine = new SimulationEngine(map,30);//30
 //        engine.run();
 
 //        RUN WITH GUI
-
+//        SaveToFile save = new SaveToFile();
+       // save.CreateFile();
         Application.launch(App.class, args);
     }
 }
